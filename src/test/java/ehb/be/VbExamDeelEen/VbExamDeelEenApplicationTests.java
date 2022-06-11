0175e9674f17e2e3759b53acfbd6084e1456b772
Soufiane Hamoumi <soufiane.hamoumi@student.ehb.be>
2022-06-11 22:41:20 +0200

    Initial commit

---
+++ src/test/java/ehb/be/VbExamDeelEen/VbExamDeelEenApplicationTests.java
@@ -0,0 +1,13 @@
+package ehb.be.VbExamDeelEen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VbExamDeelEenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
